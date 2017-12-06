54467118f7b9c4d1318a1cab793582e20897cb1b
Carlos <JerichoFrancis.Carlos@misys.com>
2017-12-06 16:38:44 +0800

    dips notification, fix valid time

--- rates.py
+++ rates.py
@@ -4,43 +4,69 @@ import time
 import playsound
 from threading import Thread
 
-# profile
-isLastBuy = True                     # buy/sell BTC
+# profile: change every transaction made then rerun script
+isLastBuy = False                     # buy/sell BTC
 amountUsed = int(2000)               # last amount used in buy/sell
-lastRate =  int(596450)              # last rate used in buy/sell
+lastRate =  int(600894)              # last rate used in buy/sell
 lastBTC = float(0.00335317)          # last BTC gain/sold
 
+highestSellRate = lastRate
+lowestBuyRate = lastRate
+    
 def printing(buyRate, sellRate):
+    global highestSellRate
+    global lowestBuyRate
+
     if isLastBuy:
         if int(sellRate) > lastRate:
-            print buyRate + "       " + sellRate + " >>>> " + str(lastRate) + "      SEEEEELL NOW"
+            print (buyRate + "       " + sellRate + " >>>> " + str(lastRate) + "      SEEEEELL NOW"),
         else:
-            print buyRate + "       " + sellRate + "      " + str(lastRate) + "      HODL"
+            print (buyRate + "       " + sellRate + "      " + str(lastRate) + "      HODL"),
     else:
         if int(buyRate) < lastRate:
-            print buyRate + " <<<<  " + sellRate + "      " + str(lastRate) + "      BUUUUUY NOW"
+            print (buyRate + " <<<<  " + sellRate + "      " + str(lastRate) + "      BUUUUUY NOW"),
         else:
-            print buyRate + "       " + sellRate + "      " + str(lastRate) + "      ..."
+            print (buyRate + "       " + sellRate + "      " + str(lastRate) + "      ..."),
+    if highestSellRate < sellRate:
+        highestSellRate = sellRate
+        print ("         $$$$$  ALL TIME HIGH BITCHES  $$$$$"),
+        playsound.playsound('mario.mp3', True)
+    if lowestBuyRate > buyRate: lowestBuyRate = buyRate
 
+    # dip detection, for PHP only
+    if ((int(highestSellRate) - int(buyRate))/1000)  > 0:
+        print ("         D"),
+        for i in range( 0, ((int(highestSellRate) - int(buyRate))/1000)):
+            print ("I"),
+        print ("P"),
+        playsound.playsound('wololo.mp3', True)
+    print ""
+    
 def rateComp(buy, sell):
     # if selling , if buying
     return isLastBuy and sell > lastRate or not isLastBuy and buy < lastRate
 
 def livethread():
     validTime = 0
+
     while True:
-        # get rates from api
-        rates = json.load(urllib2.urlopen("https://quote.coins.ph/v1/markets/BTC-PHP"))
+        try:
+            # get rates from api
+            rates = json.load(urllib2.urlopen("https://quote.coins.ph/v1/markets/BTC-PHP"))
+        except Exception:
+            print "API failed us"
+            next
 
         buyRate = rates['market']['ask']
         sellRate = rates['market']['bid']
+        validTime = rates['market']['expires_in_seconds']
 
-        printing(buyRate, sellRate)        
+        if int(validTime) > 0:
+            printing(buyRate, sellRate)        
 
         if rateComp(int(buyRate), int(sellRate)):
             playsound.playsound('buy.mp3', True)
 
-        validTime = rates['market']['expires_in_seconds']
         time.sleep(validTime)
 
 
---
+++ start.bat
@@ -0,0 +1,1 @@
+python rates.py
