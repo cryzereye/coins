943bdb82dd2d87a94766e9ecb7abde31ce16417a
Carlos <JerichoFrancis.Carlos@misys.com>
2017-12-12 02:03:32 +0800

    clean up + additional functions for v2 + README update

--- kb.py
+++ kb.py
@@ -2,12 +2,15 @@ import msvcrt
 import os
 import sys
 
+def restart():
+    clear = lambda: os.system('cls')
+    clear()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
 def read_stop():
     while True:
         try:
             if msvcrt.getch() == '~':
-                clear = lambda: os.system('cls')
-                clear()
-                os.execl(sys.executable, sys.executable, *sys.argv)
+                restart()
         except Exception:
             next
--- loading.py
+++ loading.py
@@ -1,30 +1,5 @@
 import json
 
-def jsonload():
-    # for loading recent history
-    try:
-        data = json.load("hist.json")
-        return data
-    except Exception:
-        return []
-
-def jsonsave(data):
-    # for saving the recently accumulated data, will overwrite all
-    save = {}
-    for d in data:
-        jsondata = {'datetime' : d[0],
-                    'buy'      : d[1],
-                    'sell'     : d[2]
-                   }
-        save.update(jsondata)
-    try:
-        jsonfile = open("hist.json", "w+")
-        jsonfile.write(json.dumps(save))
-        jsonfile.close()
-        return True
-    except Exception:
-        return False
-
 def updateATH(buy, sell):
     data = {'ath_buy'   : buy,
             'ath_sell'  : sell
@@ -41,4 +16,44 @@ def getATH():
         data = json.load(open("ath.json"))
         return data
     except Exception:
+        return []
+
+def save_rates(rates):
+    while True:
+        try:
+            jsonfile = open("rates.json", "w+")
+            jsonfile.write(json.dumps(rates))
+            jsonfile.close()
+            break
+        except Exception:
+            pass
+
+def get_rates():
+    try:
+        data = json.load(open("profile.json"))
+        return data
+    except Exception:
+        return []
+
+def save_profile(lastAction, rate, fiat, crypto):
+    data = {'action' : lastAction,
+            'rate' : rate,
+            'fiat' : fiat,
+            'crypto' : crypto
+           }
+    while True:
+        try:
+            jsonfile = open("profile.json", "w+")
+            jsonfile.write(json.dumps(data))
+            jsonfile.close()
+            break
+        except Exception:
+            pass
+
+def get_profile():
+    try:
+        data = json.load(open("profile.json"))
+        return data
+    except Exception:
+        print "profile.json not found!"
         return []
